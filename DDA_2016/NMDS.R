ad6bf65c32b9e6c1d25e8b98a16d86311e559b2a
Rhonda Elliott <Rhondae@taylorshellfish.com>
2017-04-29 07:40:54 -0700

    DDA upload

---
+++ DDA_2016/NMDS.R
@@ -0,0 +1,100 @@
+install.packages("vegan")
+library(vegan)
+install.packages("raster")
+library(raster)
+
+#I used the GUI interface, looked on Packages Tab to find Install Packages. Then I selected BioStatR.
+#Then I loaded the BioStatR package.
+
+source('~/GitHub/Fish-546-Bioinformatics/analyses/DDA_2016/biostats.R', encoding = 'UTF-8')
+
+cg.reps<-read.csv('/Users/rhondae/Documents/Github/Fish-546-Bioinformatics/analyses/DDA_2016/ABACUS_ADJNSAF_NMDS.csv',header=T,row.names=1)
+
+pool0<-cbind(cg.reps[1],cg.reps[2])
+S2CD3<-cbind(cg.reps[3],cg.reps[4])
+S3CD3<-cbind(cg.reps[5],cg.reps[6])
+S9HD3<-cbind(cg.reps[7],cg.reps[8])
+S2CD5<-cbind(cg.reps[9],cg.reps[10])
+S3CD5<-cbind(cg.reps[11],cg.reps[12])
+S9HD5<-cbind(cg.reps[13],cg.reps[14])
+S2CD7<-cbind(cg.reps[15],cg.reps[16])
+S3CD7<-cbind(cg.reps[17],cg.reps[18])
+S9HD7<-cbind(cg.reps[19],cg.reps[20])
+S2CD9<-cbind(cg.reps[21],cg.reps[22])
+S3CD9<-cbind(cg.reps[23],cg.reps[24])
+S9HD9<-cbind(cg.reps[25],cg.reps[26])
+S2CD11<-cbind(cg.reps[27],cg.reps[28])
+S3CD11<-cbind(cg.reps[29],cg.reps[30])
+S9HD11<-cbind(cg.reps[31],cg.reps[32])
+S2CD13<-cbind(cg.reps[33],cg.reps[34])
+S3CD13<-cbind(cg.reps[35],cg.reps[36])
+S9HD13<-cbind(cg.reps[37],cg.reps[38])
+S2CD15<-cbind(cg.reps[39],cg.reps[40])
+S3CD15<-cbind(cg.reps[41],cg.reps[42])
+S9HD15<-cbind(cg.reps[43],cg.reps[44])
+
+pool0.cv<-apply(pool0,1,cv)
+S2CD3.cv<-apply(S2CD3,1,cv)
+S3CD3.cv<-apply(S3CD3,1,cv)
+S9HD3.cv<-apply(S9HD3,1,cv)
+S2CD5.cv<-apply(S2CD5,1,cv)
+S3CD5.cv<-apply(S3CD5,1,cv)
+S9HD5.cv<-apply(S9HD5,1,cv)
+S2CD7.cv<-apply(S2CD7,1,cv)
+S3CD7.cv<-apply(S3CD7,1,cv)
+S9HD7.cv<-apply(S9HD7,1,cv)
+S2CD9.cv<-apply(S2CD9,1,cv)
+S3CD9.cv<-apply(S3CD9,1,cv)
+S9HD9.cv<-apply(S9HD9,1,cv)
+S2CD11.cv<-apply(S2CD11,1,cv)
+S3CD11.cv<-apply(S3CD11,1,cv)
+S9HD11.cv<-apply(S9HD11,1,cv)
+S2CD13.cv<-apply(S2CD13,1,cv)
+S3CD13.cv<-apply(S3CD13,1,cv)
+S9HD13.cv<-apply(S9HD13,1,cv)
+S2CD15.cv<-apply(S2CD15,1,cv)
+S3CD15.cv<-apply(S3CD15,1,cv)
+S9HD15.cv<-apply(S9HD15,1,cv)
+
+oyster.cv<-cbind(pool0.cv,S2CD3.cv,S3CD3.cv,S9HD3.cv,S2CD5.cv,S3CD5.cv,S9HD5.cv,S2CD7.cv,S3CD7.cv,S9HD7.cv,S2CD9.cv,S3CD9.cv,S9HD9.cv,S2CD11.cv,S3CD11.cv,S9HD11.cv,S2CD13.cv,S3CD13.cv,S9HD13.cv,S2CD15.cv,S3CD15.cv,S9HD15.cv)
+
+boxplot(oyster.cv,outline=T,names=c('pool0','S2CD3','S3CD3','S9HD3','S2CD5','S3CD5','S9HD5','S2CD7','S3CD7','S9HD7','S2CD9','S3CD9','S9HD9','S2CD11','S3CD11','S9HD11','S2CD13','S3CD13','S9HD13','S2CD15','S3CD15','S9HD15'))
+
+
+
+#NMDS
+reps.t<-t(cg.reps)
+reps.tra<-(reps.t+1)
+reps.tra<-data.trans(reps.tra, method='log', plot=F)
+
+#assign colors to reps
+reps.nmds<-metaMDS(reps.tra, distance='bray', k=2, trymax=100, autotransform=F)
+fig.reps<-ordiplot(reps.nmds, choices=c(1,2), type='none', display='sites', xlab='Axis 1', ylab='Axis 2', cex=0.5)
+points(fig.reps, 'sites', col=c(rep('black',2), rep('red',2), rep('red',2), rep('red',2), rep('orange',2), rep('orange',2), rep('orange',2),rep('yellow',2), rep('yellow',2), rep('yellow',2), rep('green',2),rep('green',2),rep('green',2), rep('blue',2),rep('blue',2),rep('blue',2),rep('purple',2),rep('purple',2),rep('purple',2),rep('magenta',2),rep('magenta',2),rep('magenta',2)), pch=c(rep(18,2), rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2),rep(17,2), rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2), rep(17,2),rep(19,2), rep(15,2), rep(17,2), rep(19,2), rep(15,2), rep(17,2)))
+legend("topright", legend=c("pool","23C-Silo2", "23C-Silo3", "29C-Silo9"), pch=c(18,19,15,17))
+#Day 0=black, Day 3=red, Day 5=orange, Day 7=yellow, Day 9=green, Day 11=blue, Day 13=purple, Day 15=magenta
+#pool0= diamonds, 23C-Silo 2 = circles, 23C-Silo 3= square, 29-Silo 9 = triangles
+
+#paired t-test
+t.test(cg.reps[,1],cg.reps[,2],paired=T)
+t.test(cg.reps[,3],cg.reps[,4],paired=T)
+t.test(cg.reps[,5],cg.reps[,6],paired=T)
+t.test(cg.reps[,7],cg.reps[,8],paired=T)
+t.test(cg.reps[,9],cg.reps[,10],paired=T)
+t.test(cg.reps[,11],cg.reps[,12],paired=T)
+t.test(cg.reps[,13],cg.reps[,14],paired=T)
+t.test(cg.reps[,15],cg.reps[,16],paired=T)
+t.test(cg.reps[,17],cg.reps[,18],paired=T)
+t.test(cg.reps[,19],cg.reps[,20],paired=T)
+t.test(cg.reps[,21],cg.reps[,22],paired=T)
+t.test(cg.reps[,23],cg.reps[,24],paired=T)
+t.test(cg.reps[,25],cg.reps[,26],paired=T)
+t.test(cg.reps[,27],cg.reps[,28],paired=T)
+t.test(cg.reps[,29],cg.reps[,30],paired=T)
+t.test(cg.reps[,31],cg.reps[,32],paired=T)
+t.test(cg.reps[,33],cg.reps[,34],paired=T)
+t.test(cg.reps[,35],cg.reps[,36],paired=T)
+t.test(cg.reps[,37],cg.reps[,38],paired=T)
+t.test(cg.reps[,39],cg.reps[,40],paired=T)
+t.test(cg.reps[,41],cg.reps[,42],paired=T)
+t.test(cg.reps[,43],cg.reps[,44],paired=T)
