b68cd9c3c44bfb54822f5f3e23a3a66c8cfa177a
Rhonda Elliott <Rhondae@taylorshellfish.com>
2017-07-16 10:11:21 -0700

    new files

---
+++ DIA_2015/unfoldtxt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sed 's/; /\t/g' /c/Users/Rhonda/Desktop/Backup/Documents/GitHub/project-pacific.oyster-larvae/DIA_2015/AnnotatedproteinsGOarea.txt > /c/Users/Rhonda/Desktop/Backup/Documents/GitHub/project-pacific.oyster-larvae/DIA_2015/AnnotatedproteinsGOunfoldedarea.txt
+
+file="/c/Users/Rhonda/Desktop/Backup/Documents/GitHub/project-pacific.oyster-larvae/DIA_2015/AnnotatedproteinsGOunfoldedarea.txt"
+while read -r line
+	do
+	max_field=$(echo "$line" | awk -F'\t' '{print NF}')
+	set_fields=$(echo "$line" | cut -f1-12)
+	if (( "$max_field" < 13 ))
+		then printf "%s\n" "$line"
+		else
+			goterms=$(echo "$line" | cut -f13-$max_field)
+			IFS=$'\t' read -r -a array <<<"$goterms"
+				for element in "${!array[@]}"	
+					do printf "%s\t%s\n" "$set_fields" "${array[$element]}"
+				done
+	fi
+done < "$file" > unfoldedarea.tab
