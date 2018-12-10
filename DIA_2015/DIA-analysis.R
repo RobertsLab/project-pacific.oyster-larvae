b87a646b88cfa2182656a5b6fc9d06727889af2b
Grace Crandall <graceac9@uw.edu>
2018-12-10 12:41:37 -0800

    DIA analysis R script

---
+++ DIA_2015/DIA-analysis.R
@@ -0,0 +1,6 @@
+#read in .txt file from http://owl.fish.washington.edu/scaphapoda/grace/2015-Cgseed-DIA/EncyclopeDIA-Walnut-outputs/20181210-Cg-quant-reps.elib.peptides.txt
+
+quantrep <- read.table("http://owl.fish.washington.edu/scaphapoda/grace/2015-Cgseed-DIA/EncyclopeDIA-Walnut-outputs/20181210-Cg-quant-reps.elib.peptides.txt", header = TRUE)
+
+
+
