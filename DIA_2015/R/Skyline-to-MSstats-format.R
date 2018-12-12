b0c57979f60801c414255180059720166b66c6e7
Grace Crandall <graceac9@uw.edu>
2018-12-12 15:54:31 -0800

    add ms stats

---
+++ DIA_2015/R/Skyline-to-MSstats-format.R
@@ -0,0 +1,9 @@
+library(MSstats)
+
+#read in .csv Skyline report from: http://owl.fish.washington.edu/scaphapoda/grace/2015-Cgseed-DIA/post-EncyclopeDIA-Skyline/20181212-SkytoMSstats-report.csv 
+# file is larger than 100MB 
+
+quant <- read.csv("http://owl.fish.washington.edu/scaphapoda/grace/2015-Cgseed-DIA/post-EncyclopeDIA-Skyline/20181212-SkytoMSstats-report.csv")
+
+?SkylinetoMSstatsFormat
+
