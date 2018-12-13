7796acf3274179d5e655685e31cf77584cc7f051
Grace Crandall <graceac9@uw.edu>
2018-12-13 12:01:44 -0800

    ms stats logs and edits to r scripts

--- DIA_2015/R/2015Cgseed-MSstats.R
+++ DIA_2015/R/2015Cgseed-MSstats.R
@@ -2,3 +2,8 @@
 
 MSstats <- read.csv("DIA_2015/analyses/20181213-MSstats-report.csv")
 
+MSstats.2 <- dataProcess(MSstats, normalization = "equalizeMedians", summaryMethod = "TMP", cutoffCensored = "minFeature", censoredInt = "0", MBimpute = TRUE, maxQuantileforCensored = 0.999)
+
+#** There are 4379 intensities which are zero or less than 1. These intensities are replaced with 1.
+#Error in dataProcess(MSstats, normalization = "equalizeMedians", summaryMethod = "TMP",  : 
+# ** MSstats suspects that there are fractionations and potentially technical replicates too. Please add Fraction column in the input.
--- DIA_2015/R/Skyline-to-MSstats-format.R
+++ DIA_2015/R/Skyline-to-MSstats-format.R
@@ -1,15 +1,24 @@
 library(MSstats)
 
+### IMPORT AND PROCESS DATA ###
+
 #read in .csv Skyline report from: http://owl.fish.washington.edu/scaphapoda/grace/2015-Cgseed-DIA/post-EncyclopeDIA-Skyline/20181213-SkytoMSstats-report.csv 
 # File is too large to upload to GitHub
 
-quant <- read.csv("http://owl.fish.washington.edu/scaphapoda/grace/2015-Cgseed-DIA/post-EncyclopeDIA-Skyline/20181213-SkytoMSstats-report.csv")
+rawPeakAreas <- read.csv("http://owl.fish.washington.edu/scaphapoda/grace/2015-Cgseed-DIA/post-EncyclopeDIA-Skyline/20181213-SkytoMSstats-report.csv")
 
-head(quant)
+head(rawPeakAreas)
 
-?SkylinetoMSstatsFormat
+#rename columns 
+names(rawPeakAreas) <- c("ProteinName", "PeptideSequence", "PeptideModifiedSequence", "PrecursorCharge", "PrecursorMz", "FragmentIon", "ProductCharge", "ProductMz", "IsotopeLabelType", "Condition", "BioReplicate", "FileName", "Area", "StandardType", "Truncated", "DetectionQValue")
 
-quant2 <- SkylinetoMSstatsFormat(quant, annotation = NULL, removeiRT = TRUE, filter_with_Qvalue = TRUE, qvalue_cutoff = 0.01, useUniquePeptide = TRUE, fewMeasurements = "remove", removeOxidationMpeptides = FALSE, removeProtein_with1Feature = FALSE)
+head(rawPeakAreas)
+
+#convert raw Skyline output to MSstats format
+#rawPeakAreas has 563192 obs and 16 variables
+PeakAreas <- SkylinetoMSstatsFormat(rawPeakAreas)
+
+# PeakAreas has 152980 obs, and 15 variables
 
 #** Peptides, that are used in more than one proteins, are removed.
 #** Intensities with great than 0.01 in DetectionQValue are replaced with zero.
@@ -19,6 +28,16 @@ quant2 <- SkylinetoMSstatsFormat(quant, annotation = NULL, removeiRT = TRUE, fil
  # In SkylinetoMSstatsFormat(quant, annotation = NULL, removeiRT = TRUE,  :
                               #NAs introduced by coercion
 
-head(quant2)
+head(PeakAreas)
 
-write.csv(quant2, "DIA_2015/analyses/20181213-MSstats-report.csv")
+# Process data
+QuantData <- dataProcess(PeakAreas, normalization='equalizeMedians',
+                         summaryMethod="TMP",
+                         cutoffCensored="minFeature", censoredInt="0",
+                         MBimpute=TRUE,
+                         maxQuantileforCensored=0.999)
+#** There are 4379 intensities which are zero or less than 1. These intensities are replaced with 1.
+#Error in dataProcess(PeakAreas) : 
+  #** MSstats suspects that there are fractionations and potentially technical replicates too. Please add Fraction column in the input.
+
+
