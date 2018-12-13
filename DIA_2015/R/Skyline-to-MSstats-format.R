d0b24507c80317744f20a3861c82cdd41db391ef
Grace Crandall <graceac9@uw.edu>
2018-12-13 11:18:23 -0800

    convert to MS stats format

--- DIA_2015/R/Skyline-to-MSstats-format.R
+++ DIA_2015/R/Skyline-to-MSstats-format.R
@@ -9,5 +9,14 @@ head(quant)
 
 ?SkylinetoMSstatsFormat
 
+quant2 <- SkylinetoMSstatsFormat(quant, annotation = NULL, removeiRT = TRUE, filter_with_Qvalue = TRUE, qvalue_cutoff = 0.01, useUniquePeptide = TRUE, fewMeasurements = "remove", removeOxidationMpeptides = FALSE, removeProtein_with1Feature = FALSE)
 
+#** Peptides, that are used in more than one proteins, are removed.
+#** Intensities with great than 0.01 in DetectionQValue are replaced with zero.
+#** 6 features have all NAs or zero intensity values and are removed.
+#** 818 features have 1 or 2 intensities across runs and are removed.
+#Warning message:
+ # In SkylinetoMSstatsFormat(quant, annotation = NULL, removeiRT = TRUE,  :
+                              #NAs introduced by coercion
 
+head(quant2)
